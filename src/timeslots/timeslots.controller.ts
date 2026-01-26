34e81bd87711055df3c22fa257523e7b53f60a2c
liptwo <142609541+liptwo@users.noreply.github.com>
2026-01-26 21:59:25 +0700

    add 2 route get_tl_by_host generate_tl

--- src/timeslots/timeslots.controller.ts
+++ src/timeslots/timeslots.controller.ts
@@ -1,11 +1,48 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  UseGuards,
+  ParseUUIDPipe,
+} from '@nestjs/common';
 import { TimeslotsService } from './timeslots.service';
+import { GenerateTimeslotsDto } from './dto/generate-timeslots.dto';
+import { CurrentUser, Roles } from '../common/decorators';
+import { User } from '../users/entities/user.entity';
+import { UserRole } from '../common/enums';
+import { RolesGuard } from '../common/guards';
+import { SupabaseGuard } from '../supabase';
+import { QueryTimeslotsDto } from './dto/query-timeslots.dto';
+import { TimeSlot } from './entities/timeslot.entity';
 
 @Controller('timeslots')
 export class TimeslotsController {
-    constructor(private readonly timeslotsService: TimeslotsService) { }
+  constructor(private readonly timeslotsService: TimeslotsService) {}
 
-    // POST /timeslots/generate
+  @Post('generate')
+  @UseGuards(SupabaseGuard, RolesGuard)
+  @Roles(UserRole.HOST)
+  async generateTimeSlots(
+    @Body() generateDto: GenerateTimeslotsDto,
+    @CurrentUser() currentUser: User,
+  ): Promise<{ created: number; message: string }> {
+    return this.timeslotsService.generateTimeslots(generateDto, currentUser);
+  }
 
-    // GET /timeslots?hostId=xxx
+  @Get()
+  async findAvailableSlots(
+    @Query() queryParams: QueryTimeslotsDto,
+  ): Promise<TimeSlot[]> {
+    return this.timeslotsService.findAvailableSlots(queryParams);
+  }
+
+  @Get('host/:id')
+  async findGuestTimeslotsByHost(
+    @Param('id', ParseUUIDPipe) hostId: string,
+  ): Promise<TimeSlot[]> {
+    return this.timeslotsService.findGuestTimeslotsByHost(hostId);
+  }
 }
